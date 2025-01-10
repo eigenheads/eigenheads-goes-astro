3c0b8768c7505044af15b16f336cd18db08c3549
Hannes Stiebitzhofer <hannes@stiebitzhofer.com>
2025-01-10 08:38:58 +0100

    Add languages

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,15 +15,43 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'My Docs',
+			defaultLocale: 'en', // English as default
+      locales: {
+        // English
+        en: {
+          label: 'English',
+          lang: 'en',
+        },
+        // French
+        fr: {
+          label: 'Français',
+          lang: 'fr',
+        },
+        // Spanish
+        es: {
+          label: 'Español',
+          lang: 'es',
+        },
+        // Portuguese
+        pt: {
+          label: 'Português',
+          lang: 'pt',
+        },
+        // Mandarin Chinese
+        zh: {
+          label: '中文',
+          lang: 'zh-CN',
+        },
+			},
 			social: {
-				github: 'https://github.com/withastro/starlight',
+			// github: 'https://github.com/withastro/starlight',
 			},
 			sidebar: [
 				{
 					label: 'Guides',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+				//		{ label: 'Example Guide', slug: 'guides/example' },
 					],
 				},
 				{
